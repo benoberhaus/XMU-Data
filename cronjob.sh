3bd0f09fd043ff7a2307a17eb0571c06643fb54b
Ben Oberhaus <oberhaus.12@osu.edu>
2015-09-03 17:46:38 -0400

    fix to cronjob

--- cronjob.sh
+++ cronjob.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-curr=$(ps aux | grep XMU-Data | wc -l)
+curr=$(ps aux | grep bin/XMU-Data | wc -l)
 if [[ $curr == 1 ]]; then
     datetime=$(date "+%Y-%m-%d %H:%M:%S")
     echo "$datetime - Daemon Down... Rebooting" >> ~/cron.out
