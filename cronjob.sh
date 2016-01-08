681c9fcffc1ee70585e8706e472e05579254d680
Ben Oberhaus <oberhaus.12@osu.edu>
2016-01-08 01:21:10 -0500

    modified cronjob

--- cronjob.sh
+++ cronjob.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-curr=$(ps aux | grep bin/XMU-Data | wc -l)
+curr=$(ps aux | grep bin/scraper | wc -l)
 if [[ $curr == 1 ]]; then
     datetime=$(date "+%Y-%m-%d %H:%M:%S")
     echo "$datetime - Daemon Down... Rebooting" >> ~/cron.out
-    nohup ~/go/bin/XMU-Data2 &
+    nohup ~/go/bin/scraper &
 fi
